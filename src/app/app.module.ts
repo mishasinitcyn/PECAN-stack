69496a2404819125c915469519a13d9fe35cced9
Mikhail Sinitcyn <msa242@sfu.ca>
2024-08-02 21:40:52 -0700

    INIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,45 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+registerLocaleData(en);
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { GithubOutline, RadarChartOutline, FileMarkdownOutline, DownloadOutline, MinusOutline } from '@ant-design/icons-angular/icons';
+const icons = [GithubOutline, RadarChartOutline, FileMarkdownOutline, DownloadOutline, MinusOutline];
+
 
 @NgModule({
   declarations: [
@@ -10,11 +47,35 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    NzButtonModule,
+    NzInputModule,
+    NzTabsModule,
+    NzCardModule,
+    NzTableModule,
+    NzTagModule,
+    NzPageHeaderModule,
+    NzAlertModule,
+    NzNotificationModule,
+    NzSpaceModule,
+    NzFlexModule,
+    NzSpinModule,
+    NzListModule,
+    NzGridModule,
+    NzLayoutModule,
+    NzIconModule.forRoot(icons),
+    NzModalModule,
+    NzTypographyModule,
+    NzAvatarModule,
+    NzToolTipModule,
+    ClipboardModule,
+    NzMessageModule,
+    RouterModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.prod.template.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    apiUrl: '${API_URL}',
+    production: true,
+
+    // Github OAuth
+    githubClientId: '${GITHUB_CLIENT_ID}',
+    githubRedirectUri: '${GITHUB_REDIRECT_URI}',
+    githubClientSecret: '${GITHUB_CLIENT_SECRET}'
+  };
